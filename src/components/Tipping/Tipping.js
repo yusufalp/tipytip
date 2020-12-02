b9d98161c6daae513e57b102a63b6dee53d51e0e
Yusuf Alp <alpyusuf007@gmail.com>
2020-12-01 20:21:27 -0800

    Added two inputs, percent and amount of tip

--- src/components/Tipping/Tipping.js
+++ src/components/Tipping/Tipping.js
@@ -4,8 +4,9 @@ import './Tipping.css';
 
 class Tipping extends React.Component {
   state = {
-    amount: 0,
-    percent: 15,
+    amount: 1,
+    percent: 0,
+    tip: 1,
     isCalculated: false,
     percentError: '',
     amountError: ''
@@ -32,7 +33,7 @@ class Tipping extends React.Component {
       amount: value
     })
   }
-  handlePercentChange = (value) => {
+  handleTipPercentChange = (value) => {
     if (value < 0 || value > 100) {
       this.setState({
         percentError: 'Must be between 1 and 100'
@@ -47,13 +48,17 @@ class Tipping extends React.Component {
       percent: value
     })
   }
+  handleTipAmountChange = (value) => {
+    this.setState({
+      tip: value
+    })
+  }
   render() {
-    console.log(this.state.percent)
     return (
       <div>
         <h1>Tip</h1>
         <form>
-          <label htmlFor='/amount'>
+          <label htmlFor='amount'>
             Amount
           <input
               id='amount'
@@ -66,24 +71,32 @@ class Tipping extends React.Component {
             />
           </label>
           <p className={this.state.amountError ? '' : 'hidden'}>{this.state.amountError}</p>
-          <label htmlFor='/amount'>
-            Enter Percent
+          <label htmlFor='tip-percent'>
+            If you tip
           <input
-              id='amount'
+              id='tip-percent'
               type='number'
               value={this.state.percent}
               min='0'
               max='100'
-              onChange={e => this.handlePercentChange(e.target.value)}
+              onChange={e => this.handleTipPercentChange(e.target.value)}
               required
-            />
+            /> %, your tip will be {Math.floor(this.state.amount * this.state.percent) / 100}
+          </label>
+          <label htmlFor='tip-amount'>
+            If you tip
+          <input
+              id='tip-amount'
+              type='number'
+              value={this.state.tip}
+              min='0'
+              step='.01'
+              onChange={e => this.handleTipAmountChange(e.target.value)}
+              required
+            /> $, that is {(Math.floor(this.state.tip / this.state.amount * 10000)) / 100}% of your bill
           </label>
           <p className={this.state.percentError ? '' : 'hidden'}>{this.state.percentError}</p>
-          <button type='submit' onClick={e => this.showResult(e)}>Calculate</button>
         </form>
-        <div className={this.state.isCalculated ? '' : 'hidden'}>
-          <Result amount={this.state.amount} percent={this.state.percent} />
-        </div>
       </div>
     );
   }
