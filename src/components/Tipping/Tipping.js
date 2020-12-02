58330c4491b03208dcfd437df1302432aa0fbe4c
Yusuf Alp <alpyusuf007@gmail.com>
2020-12-01 20:02:13 -0800

    Added input validation for required range

--- src/components/Tipping/Tipping.js
+++ src/components/Tipping/Tipping.js
@@ -6,21 +6,43 @@ class Tipping extends React.Component {
   state = {
     amount: 0,
     percent: 15,
-    isCalculated: false
+    isCalculated: false,
+    percentError: '',
+    amountError: ''
   }
   showResult = (e) => {
     e.preventDefault();
     this.setState({
       isCalculated: true
     })
-    
+
   }
   handleAmountChange = (value) => {
+    if (value < 0) {
+      this.setState({
+        amountError: 'Must be a positive number'
+      })
+      return
+    } else {
+      this.setState({
+        amountError: ''
+      })
+    }
     this.setState({
       amount: value
     })
   }
   handlePercentChange = (value) => {
+    if (value < 0 || value > 100) {
+      this.setState({
+        percentError: 'Must be between 1 and 100'
+      })
+      return
+    } else {
+      this.setState({
+        percentError: ''
+      })
+    }
     this.setState({
       percent: value
     })
@@ -40,21 +62,27 @@ class Tipping extends React.Component {
               min='0'
               step='.01'
               onChange={e => this.handleAmountChange(e.target.value)}
+              required
             />
           </label>
+          <p className={this.state.amountError ? '' : 'hidden'}>{this.state.amountError}</p>
           <label htmlFor='/amount'>
             Enter Percent
           <input
               id='amount'
               type='number'
               value={this.state.percent}
+              min='0'
+              max='100'
               onChange={e => this.handlePercentChange(e.target.value)}
+              required
             />
           </label>
+          <p className={this.state.percentError ? '' : 'hidden'}>{this.state.percentError}</p>
           <button type='submit' onClick={e => this.showResult(e)}>Calculate</button>
         </form>
         <div className={this.state.isCalculated ? '' : 'hidden'}>
-          <Result amount={this.state.amount} percent={this.state.percent}/>
+          <Result amount={this.state.amount} percent={this.state.percent} />
         </div>
       </div>
     );
