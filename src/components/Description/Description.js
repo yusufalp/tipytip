ea17bdf8b17a706305d22cfb4a05b91ef7e3f50d
Yusuf Alp <alpyusuf007@gmail.com>
2020-12-05 07:06:06 -0800

    Added a new component for description

---
+++ src/components/Description/Description.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Description() {
+  return (
+    <div>
+      <p>Sure you tip! Do you calculate by hand? In your head? Calculator?</p>
+      <p>What if you do not want to bother and just tip whatever bill you get
+    out of your wallet or pocket? What if you are in a hurry?</p>
+      <p>Then you go home, relax, think about your day and suddenly remember
+      your tip. How much did you tip? Yes, but was that enough? Were you fair
+      to the service you received? Really, how much percent that $20 you just tipped
+    without thinking is?</p>
+      <p>Wonder no more!</p>
+    </div>
+  )
+}
